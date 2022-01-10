8f96759bef963a7c47cee332a56eecc996717a83
roneal22 <90431203+roneal22@users.noreply.github.com>
2022-01-10 14:14:27 -0800

    done

--- ClientSide.js
+++ ClientSide.js
@@ -1,19 +1,15 @@
-alert("hello");
+alert("hello!");
 discount = 0;
 tax = 6.25;
 taxString = "";
-// numDogs, numFries, numDrinks;
-// subtotal, tax, grandTotal;
 
-document.write("Welcome to Joe's!" + "<br><br>");
+
+document.write("<b>Welcome to Joe's!</b><br><br>");
 
 numDogs = prompt("how many hotdogs?", 0);
 subtotal = (numDogs * 3.75);
-alert("numDogs" + numDogs);
-alert("subtotal" + numDogs);
 numFries = prompt("how many fries?", 0);
 subtotal += numFries * 2.00;
-alert("here " + subtotal);
 numDrinks = prompt("how many drinks?", 0);
 subtotal += numDrinks * 1.5;
 
@@ -22,11 +18,10 @@ document.writeln("Fries: " + numFries + "<br>");
 document.writeln("Drinks: " + numDrinks + "<br>");
 document.writeln("\nSubtotal: $" + subtotal + "<br>");
 
-alert("here" + subtotal);
 if(subtotal >= 20){
 	discount = (subtotal * .1);
 	discount = discount.toFixed(2);
-	document.write("discount: -$" + discount + "<br>");
+	document.write("<i>discount: -$" + discount + "<br></i>");
 	subtotal -= discount;
 }
 
