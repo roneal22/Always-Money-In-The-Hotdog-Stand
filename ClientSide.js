68a54a468a5f8bdcbd34539cb77ca8c6100c2a55
roneal22 <90431203+roneal22@users.noreply.github.com>
2022-01-10 14:02:09 -0800

    a

--- ClientSide.js
+++ ClientSide.js
@@ -2,33 +2,37 @@ alert("hello");
 discount = 0;
 tax = 6.25;
 taxString = "";
-var numDogs, numFries, numDrinks;
-var subtotal, tax, grandTotal;
+// numDogs, numFries, numDrinks;
+// subtotal, tax, grandTotal;
 
-document.write("Welcome to Joe's!");
+document.write("Welcome to Joe's!" + "<br><br>");
 
 numDogs = prompt("how many hotdogs?", 0);
-subtotal += numDogs * 3.75;
+subtotal = (numDogs * 3.75);
+alert("numDogs" + numDogs);
+alert("subtotal" + numDogs);
 numFries = prompt("how many fries?", 0);
 subtotal += numFries * 2.00;
+alert("here " + subtotal);
 numDrinks = prompt("how many drinks?", 0);
 subtotal += numDrinks * 1.5;
 
-document.writeln("Hotdogs: " + numDogs);
-document.writeln("Fries: " + numFries);
-document.writeln("Drinks: " + numDrinks);
-document.writeln("\nSubtotal: $" + subtotal);
+document.writeln("Hotdogs: " + numDogs + "<br>");
+document.writeln("Fries: " + numFries + "<br>");
+document.writeln("Drinks: " + numDrinks + "<br>");
+document.writeln("\nSubtotal: $" + subtotal + "<br>");
 
+alert("here" + subtotal);
 if(subtotal >= 20){
-	discount = subtotal * .1;
+	discount = (subtotal * .1);
 	discount = discount.toFixed(2);
-	document.write("discount: -$" + discount);
+	document.write("discount: -$" + discount + "<br>");
 	subtotal -= discount;
 }
 
-tax = subtotal * .0625;
+tax = (subtotal * .0625);
 taxString = tax.toFixed(2);
-document.writeln("Meals Tax: $" + taxString);
+document.writeln("Meals Tax: $" + taxString + "<br>");
 grandTotal = subtotal + tax;
 grandTotal = grandTotal.toFixed(2);
-document.writeln("Grand Total: $" + grandTotal);
+document.writeln("Grand Total: $" + grandTotal + "<br>");
